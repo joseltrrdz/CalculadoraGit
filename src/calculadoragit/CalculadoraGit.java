3ee98de6e7e3f35babf324f221586e5ab6a73901
Jose <joselatorredomnguez@gmail.com>
2023-05-30 16:53:42 +0200

    SumaCreada

--- src/calculadoragit/CalculadoraGit.java
+++ src/calculadoragit/CalculadoraGit.java
@@ -14,7 +14,11 @@ public class CalculadoraGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    }
+    
+    public static int Resta(int numUno, int numDos){
+        return numUno-numDos;
     }
     
 }
